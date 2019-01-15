c9fb834b2e6065c0a46f7d05348db4dde993894c
Chris Pfaff <chrispfaff10@gmail.com>
2019-01-14 20:16:21 -0600

    set up project to run a node server backend

--- modernJavascriptBoilerplate/src/App.js
+++ modernJavascriptBoilerplate/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
+import './styles.css'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1 />
+      <div className="container">
+        <h1>Red Rocket Driver Location Challenge</h1>
       </div>
     )
   }
--- modernJavascriptBoilerplate/src/server.js
+++ modernJavascriptBoilerplate/src/server.js
@@ -1,7 +1,11 @@
-import express from 'express'
+const express = require('express')
 
 const app = express()
 const port = 3001
+
+app.get('/', (req, res) => {
+  console.log('here')
+})
 
 app.listen(port, () => {
   console.log(`server is listening on port number ${port}`)
