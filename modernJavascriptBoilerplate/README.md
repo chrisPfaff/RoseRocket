d02efb3539b5851bc767ae55b843f50fe6289464
Chris Pfaff <chrispfaff10@gmail.com>
2019-01-14 18:46:37 -0600

    setting up test project

---
+++ modernJavascriptBoilerplate/README.md
@@ -0,0 +1,8 @@
+# React Javascript Boilerplate with Babel/Webpack
+
+```javascript
+'git clone --depth-1 <this repo url> <new directory>'
+'ls -a'
+'rm -rf .git'
+'git init'
+```
---
+++ modernJavascriptBoilerplate/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '@babel/polyfill'
+
+import App from './App'
+import './styles.css'
+import DefaultErrorBoundary from './DefaultErrorBoundry'
+
+//will find accessability issues the linter wont catch
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DefaultErrorBoundary>
+      <App />{' '}
+    </DefaultErrorBoundary>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
